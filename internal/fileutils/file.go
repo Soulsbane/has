74336acf8f7c85f0bcb691aaf34f630a82be49b3
Paul Crane <paul@acheronsoft.com>
2022-10-26 03:11:05 -0500

    Add file internal/fileutils/file.go

---
+++ internal/fileutils/file.go
@@ -0,0 +1,42 @@
+package fileutils
+
+import (
+	"errors"
+	"os"
+	"runtime"
+)
+
+func IsFileHidden(info os.DirEntry) bool {
+	if runtime.GOOS != "windows" {
+		return info.Name()[0:1] == "."
+	}
+
+	return false
+}
+
+func FileOrPathExists(fileName string) bool {
+	if _, err := os.Stat(fileName); errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+
+	return true
+}
+
+func IsLink(mode os.FileMode) bool {
+	return mode&os.ModeSymlink != 0
+}
+
+// GetLinkPath returns the path of the link and a boolean indicating if the link destination path exists
+func GetLinkPath(name string) (string, bool) {
+	realPath, err := os.Readlink(name)
+
+	if err != nil {
+		return "", FileOrPathExists(realPath)
+	}
+
+	return realPath, FileOrPathExists(realPath)
+}
+
+func IsFileExecutable(mode os.FileMode) bool {
+	return mode&0111 != 0
+}

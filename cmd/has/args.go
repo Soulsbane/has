69c9c1b9386cf841e64ac16f9ce042035ed327da
Paul Crane <paul@acheronsoft.com>
2022-10-26 03:59:52 -0500

    Add initial argument variables to ProgramArgs type

--- cmd/has/args.go
+++ cmd/has/args.go
@@ -1,7 +1,7 @@
 package main
 
 type ProgramArgs struct {
-	FileName string `arg:"positional, required"`
-	NoPath   bool   `arg:"-n, --no-path" default:"false" help:"Include directories in user's $PATH."`
-	Ugly     bool   `arg:"-u, --ugly" default:"false" help:"Remove colorized output. Yes it's ugly."`
+	Name   string `arg:"positional, required"`
+	NoPath bool   `arg:"-n, --no-path" default:"false" help:"Include directories in user's $PATH."`
+	Ugly   bool   `arg:"-u, --ugly" default:"false" help:"Remove colorized output. Yes it's ugly."`
 }

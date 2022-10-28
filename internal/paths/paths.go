4b65f7ebc22ee79beea8d44e93779a82194ee8a9
Paul Crane <paul@acheronsoft.com>
2022-10-27 22:17:48 -0500

    Add paths.go

--- internal/paths/paths.go
+++ internal/paths/paths.go
@@ -1,1 +1,25 @@
 package paths
+
+import (
+	"os"
+	"strings"
+)
+
+func IsValidPath(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func GetAdditionalPaths() []string {
+	path, variableExists := os.LookupEnv("PATH")
+	var paths []string
+
+	if variableExists {
+		paths = strings.Split(path, ":")
+	}
+
+	return paths
+}
